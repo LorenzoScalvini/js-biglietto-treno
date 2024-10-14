c16dc8404fc9fce255b1e5a180d2436643375693
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-14 15:05:03 +0200

    Update

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ let bigliettoBase = km * 0.21
 //Pui di 65 anni sconto del 40%
 if (age < 18) {
     let sconto = bigliettoBase * 20 / 100
-    console.log("Il costo del biglietto e': " + sconto + "€")
+    let prezzoScontato = sconto
+    console.log("Il costo del biglietto e': " + prezzoScontato + "€")
 }
 else if (age > 65) {
     let sconto = bigliettoBase * 40 / 100
-    console.log("Il costo del biglietto e': " + sconto + "€")
+    let prezzoScontato = sconto
+    console.log("Il costo del biglietto e': " + prezzoScontato + "€")
 }
 else {
     console.log("Il costo del biglietto e': " + bigliettoBase + "€")
